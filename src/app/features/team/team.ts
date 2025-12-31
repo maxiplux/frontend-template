c264be8d3d27d97914e465bfcf7508be9d1bda7e
juan <maxiplux@gmail.com>
2025-12-30 21:07:27 -0500

    feat: Integrate PrimeNG by adding its documentation and updating application templates, logic, and styles.

--- src/app/features/team/team.ts
+++ src/app/features/team/team.ts
@@ -1,11 +1,46 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { PageHeader } from '../../shared/components/page-header/page-header';
 import { EmptyState } from '../../shared/components/empty-state/empty-state';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { FileUploadModule } from 'primeng/fileupload';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputTextModule } from 'primeng/inputtext';
+import { SelectModule } from 'primeng/select';
+import { TextareaModule } from 'primeng/textarea';
+import { ToggleSwitchModule } from 'primeng/toggleswitch';
 
 @Component({
   selector: 'app-team',
   templateUrl: './team.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [PageHeader, EmptyState],
+  
+  imports: [PageHeader, EmptyState, CommonModule, FormsModule, ButtonModule, FileUploadModule, InputGroupModule, InputGroupAddonModule, InputTextModule, SelectModule, TextareaModule, ToggleSwitchModule],
+
+
 })
-export class Team {}
+export class Team {
+    checked: boolean = true;
+    selectedCountry: any = null;
+    countries = signal([
+        { name: 'Afghanistan', code: 'AF' },
+        { name: 'Åland Islands', code: 'AX' },
+        { name: 'Albania', code: 'AL' },
+        { name: 'Algeria', code: 'DZ' },
+        { name: 'American Samoa', code: 'AS' },
+        { name: 'AndorrA', code: 'AD' },
+        { name: 'Angola', code: 'AO' },
+        { name: 'Anguilla', code: 'AI' },
+        { name: 'Antarctica', code: 'AQ' },
+        { name: 'Antigua and Barbuda', code: 'AG' },
+        { name: 'Argentina', code: 'AR' },
+        { name: 'Armenia', code: 'AM' },
+        { name: 'Aruba', code: 'AW' },
+        { name: 'Australia', code: 'AU' },
+        { name: 'Austria', code: 'AT' },
+        { name: 'Azerbaijan', code: 'AZ' },
+        { name: 'Bahamas', code: 'BS' }
+    ]);
+}
